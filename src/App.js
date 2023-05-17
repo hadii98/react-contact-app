1cdc1b19fd9f45a5abe6e55c527b22e53d47b1b5
hadii98 <annuaralhadi04@gmail.com>
2023-05-18 06:25:49 +0800

    add new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
